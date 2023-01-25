663f77b7c46f9cbbb62f7d047a19f03d9faadf6a
Giovanni Ciotta <giociotta@gmail.com>
2023-01-25 16:30:03 +0100

    correzioni

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -12,43 +12,40 @@ public class PasswordGenerator {
 		int giornoDiNascita;
 		int meseDiNascita;
 		int annoDiNascita;
-		
-		 Scanner s =new Scanner (System.in);
-		
-		 System.out.print("inserisci nome:  ");
-		 nome= s .nextLine();
-		
-		 System.out.print("inserisci cognome:  ");
-			cognome= s .nextLine();
-			
-			System.out.print("inserisci il tuo colore preferito:  ");
-			colorePreferito= s .nextLine();
-			
-			
-			Scanner input = new Scanner(System.in);
-			
-			System.out.print("inserisci il tuo giorno di nascita:  ");
-			giornoDiNascita = input.nextInt();
-			
-			System.out.print("inserisci il tuo mese di nascita:  ");
-			meseDiNascita= input.nextInt();
-			
-			System.out.print("inserisci il tuo anno di nascita:  ");
-			annoDiNascita= input.nextInt();
-			
-			s.close();
-			input.close();
-			
-			int somma = (giornoDiNascita + meseDiNascita +  annoDiNascita );
-			 System.out.println("nome:  " + nome);
-			 System.out.println("cognome: " + cognome );
-			 System.out.println("il tuo colore preferito : " + colorePreferito );
-			 System.out.println("giorno di nascita : " + giornoDiNascita );
-			 System.out.println("mese di nascita : " +  meseDiNascita);
-			 System.out.println("anno di nascita : " +  annoDiNascita);
-			 System.out.println("la tua password è  - : " +nome +"-"+cognome +"-"+colorePreferito+"-" + somma);
-			 
 
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("inserisci nome:  ");
+		nome = s.nextLine();
+
+		System.out.print("inserisci cognome:  ");
+		cognome = s.nextLine();
+
+		System.out.print("inserisci il tuo colore preferito:  ");
+		colorePreferito = s.nextLine();
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("inserisci il tuo giorno di nascita:  ");
+		giornoDiNascita = input.nextInt();
+
+		System.out.print("inserisci il tuo mese di nascita:  ");
+		meseDiNascita = input.nextInt();
+
+		System.out.print("inserisci il tuo anno di nascita:  ");
+		annoDiNascita = input.nextInt();
+
+		s.close();
+		input.close();
+
+		int somma = (giornoDiNascita + meseDiNascita + annoDiNascita);
+		System.out.println("nome:  " + nome);
+		System.out.println("cognome: " + cognome);
+		System.out.println("il tuo colore preferito : " + colorePreferito);
+		System.out.println("giorno di nascita : " + giornoDiNascita);
+		System.out.println("mese di nascita : " + meseDiNascita);
+		System.out.println("anno di nascita : " + annoDiNascita);
+		System.out.println("la tua password è  - : " + nome + "-" + cognome + "-" + colorePreferito + "-" + somma);
 
 	}
 
